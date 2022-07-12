a0fd81e132ee33fa1ea41a8e85cf43bca0a53ceb
siavash <siamhb@protonmail.com>
2022-07-12 13:51:40 +0430

    build a base of the project

---
+++ src/Styles/globalCss.ts
@@ -0,0 +1,38 @@
+import { globalCss } from "./@stitches.config";
+
+const globalStyle = globalCss({
+    "*":{
+        padding:"0",
+        margin:"0",
+        boxSizing:"border-box",
+        transition:"$coAndBg",
+    },
+    "::selection":{
+        backgroundColor:"$onBg100"
+    },
+    body:{
+        minHeight:"100vh",
+        // backgroundColor:"$bg"
+    },
+    "@import":'https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap',
+    "#root":{
+        // backgroundColor:"$bg",
+        minHeight:"100vh",
+        width:"100%",
+        flex_c:"",
+        jc_ac:"",
+        position:"relative",
+    },
+    "#wrapper":{
+
+
+        width:"100%",
+        flex_c:"",
+        jfs_ac:"",
+        height:"100vh",
+        minHeight:"100vh",
+
+    }
+})
+
+export default globalStyle
---
+++ src/asesst/Icons/Ico_quote.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Ico_quote (props:any){
+    return(
+      <svg id={props.id} width={props.width} height={props.height} viewBox="0 0 234 325" xmlns="http://www.w3.org/2000/svg">
+<path d="M110 114V211.5H0V114C5.31594 47.5256 39.9615 20.4336 89 0V61C57.0929 73.1507 33.9931 73.3671 44 114H56.0001H110Z" />
+<path d="M124 210.5L124 113H234L234 210.5C228.684 276.974 194.039 304.066 145 324.5V263.5C176.907 251.349 200.007 251.133 190 210.5H178H124Z" />
+</svg>
+
+    )
+}
+
+
+
+export default Ico_quote
+
